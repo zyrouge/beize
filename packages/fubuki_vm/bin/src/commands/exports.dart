241a895857fff977f07473285d053fb70c620fe2
Zyrouge <zyrouge@hotmail.com>
2022-12-28 12:23:36 +0530

    chore: cleanup

--- packages/fubuki_vm/bin/src/commands/exports.dart
+++
@@ -1,1 +0,0 @@
-export 'run.dart';
--- packages/fubuki_vm/bin/src/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:args/command_runner.dart';
-import 'commands/exports.dart';
-
-Future<void> main(final List<String> args) async {
-  final CommandRunner<void> app = CommandRunner<void>('outre', '');
-  app.addCommand(RunCommand());
-  await app.run(args);
-}
