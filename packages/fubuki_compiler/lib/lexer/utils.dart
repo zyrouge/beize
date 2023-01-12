81a1f127131e70ca0fb5c3fcb341668e2cb29307
Zyrouge <zyrouge@hotmail.com>
2023-01-12 22:44:20 +0530

    refactor: support negative powers

--- packages/fubuki_compiler/lib/lexer/utils.dart
+++ packages/fubuki_compiler/lib/lexer/utils.dart
@@ -2,7 +2,7 @@ class FubukiLexerUtils {
   static bool isWhitespace(final String char) => char.trim().isEmpty;
   static bool isNumeric(final String char) => '0123456789'.contains(char);
   static bool isNumericContent(final String char) =>
-      '0123456789.exabcdef'.contains(char.toLowerCase());
+      '-0123456789.exabcdef'.contains(char.toLowerCase());
   static bool isQuote(final String char) => char == "'" || char == '"';
   static bool isAlpha(final String char) => RegExp('[A-Za-z_]').hasMatch(char);
   static bool isAlphaNumeric(final String char) =>
