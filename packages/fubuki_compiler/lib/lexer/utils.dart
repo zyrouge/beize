3c4a0cde6136fe2cf8ceded86d80541662e003dd
Zyrouge <zyrouge@hotmail.com>
2023-01-14 23:46:13 +0530

    refactor: support $

--- packages/fubuki_compiler/lib/lexer/utils.dart
+++ packages/fubuki_compiler/lib/lexer/utils.dart
@@ -4,7 +4,8 @@ class FubukiLexerUtils {
   static bool isNumericContent(final String char) =>
       '+-0123456789.exabcdef'.contains(char.toLowerCase());
   static bool isQuote(final String char) => char == "'" || char == '"';
-  static bool isAlpha(final String char) => RegExp('[A-Za-z_]').hasMatch(char);
+  static bool isAlpha(final String char) =>
+      RegExp('[A-Za-z_\$]').hasMatch(char);
   static bool isAlphaNumeric(final String char) =>
       isAlpha(char) || isNumeric(char);
 }
