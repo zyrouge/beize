a3c07e3907d23c532f013142536bb706be4dbc90
Zyrouge <zyrouge@hotmail.com>
2023-01-16 12:47:25 +0530

    refactor: string quotes

--- packages/fubuki_compiler/lib/lexer/utils.dart
+++ packages/fubuki_compiler/lib/lexer/utils.dart
@@ -3,7 +3,8 @@ class FubukiLexerUtils {
   static bool isNumeric(final String char) => '0123456789'.contains(char);
   static bool isNumericContent(final String char) =>
       '+-0123456789.exabcdef'.contains(char.toLowerCase());
-  static bool isQuote(final String char) => char == "'" || char == '"';
+  static bool isQuote(final String char) =>
+      char == "'" || char == '"' || char == '`';
   static bool isAlpha(final String char) =>
       r'$_abcdefghijklmnopqrstuvwxyz'.contains(char.toLowerCase());
   static bool isAlphaNumeric(final String char) =>
