75fee6d8142f5910cbb4b3cfec5530c9cc6fbdff
zyrouge <zyrouge@hotmail.com>
2023-06-21 18:16:32 +0530

    chore: lint

---
+++ scripts/releaseName.js
@@ -0,0 +1,12 @@
+const { execSync } = require("child_process");
+
+const getReleaseName = () => {
+    const sha = execSync("git rev-parse HEAD").toString().substring(0, 6);
+    const now = new Date();
+    const date = now.getDate().toString().padStart(2, "0");
+    const month = (now.getMonth() + 1).toString().padStart(2, "0");
+    const year = now.getFullYear().toString();
+    return `v${year}.${month}.${date}-${sha}`;
+};
+
+module.exports = { getReleaseName };
