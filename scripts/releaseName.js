4f8054250f29f4f879a49a28ccfaf614b6dae8bf
zyrouge <zyrouge@hotmail.com>
2023-06-21 20:59:14 +0530

    chore: better tag names

--- scripts/releaseName.js
+++ scripts/releaseName.js
@@ -1,7 +1,7 @@
 const { execSync } = require("child_process");
 
 const getReleaseName = () => {
-    const sha = execSync("git rev-parse HEAD").toString().substring(0, 6);
+    const sha = execSync("git rev-parse HEAD").toString().substring(0, 7);
     const now = new Date();
     const date = now.getDate().toString().padStart(2, "0");
     const month = (now.getMonth() + 1).toString().padStart(2, "0");
